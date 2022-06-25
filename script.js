a0cdd81481cfda9ceefb776bc43e4ee5ebf582ff
fbpr <71777049+fbpr@users.noreply.github.com>
2022-06-25 19:00:36 +0700

    Remake all files

--- script.js
+++ script.js
@@ -1,42 +1,19 @@
-
-const app = document.getElementById('root');
-
-const logo = document.createElement('img');
-logo.src = 'logo.png';
-
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
-
-app.appendChild(logo);
-app.appendChild(container);
-
-var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-request.onload = function () {
-
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
-
-      const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
-
-      const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
-
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
+function newQuote() {
+  // Replace ./data.json with your JSON feed
+  fetch('https://animechan.vercel.app/api/random')
+    .then((response) => {
+      return response.json();
+    })
+    .then((quote) => {
+      document.getElementById('quote-text').innerHTML = quote.quote;
+      document.getElementById('quote-character').innerHTML = `${quote.character}, ${quote.anime}`;
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  } else {
-    const errorMessage = document.createElement('marquee');
-    errorMessage.textContent = `Gah, it's not working!`;
-    app.appendChild(errorMessage);
-  }
-}
 
-request.send();
+    setTimeout(function() {
+      var x = document.getElementById('quotes');
+      x.style.display = 'block';
+    }, 1000) 
+}
